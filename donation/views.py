69ef62f33ee58b54149a76344fbb126ab7c9d0cf
IlyasWEBX1 <l200220131@student.ums.ac.id>
2025-05-01 05:28:51 +0700

    Add files via upload

--- donation/views.py
+++ donation/views.py
@@ -38,3 +38,18 @@ def login_view(request):
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+def mitra(request):
+    return render(request, 'donation/daftarmitrarelawan.html')
+
+def donatur(request):
+    return render(request, 'donation/donasisekarang.html')
+  
+def status_donasi(request):
+    return render(request, 'donation/statusdonasi.html')
+
+def form_donasi(request):
+    return render(request, 'donation/formdonasi.html')
+
+def riwayat_donasi(request):
+    return render(request, 'donation/riwayatdonasi.html')
