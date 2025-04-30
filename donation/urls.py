a077224d5b7b95f62c848f8b40c4ed885e1b420e
DavaIlham261 <davamuhammad261@gmail.com>
2025-04-30 08:35:02 +0700

    optimalisasi
    1. Template inheritance
    2. working navigation

--- donation/urls.py
+++ donation/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mitra/', views.mitra, name='daftar_mitra'),
+    # path('mitra/', views.mitra, name='daftar_mitra'),
+    path('', views.mitra, name='home'),
     path('donasi/', views.donatur, name='donasi_sekarang'),
     path('status/', views.status_donasi, name='status_donasi'),
 ]
