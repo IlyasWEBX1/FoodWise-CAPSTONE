fe7b8737b30c010fd43b059f68f07853bf61519d
IlyasWEBX1 <l200220131@student.ums.ac.id>
2025-05-01 06:05:15 +0700

    url login dan signup

--- donation/urls.py
+++ donation/urls.py
@@ -1,11 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for food_donation project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # path('mitra/', views.mitra, name='daftar_mitra'),
-    path('', views.mitra, name='home'),
-    path('donasi/', views.donatur, name='donasi_sekarang'),
-    path('status/', views.status_donasi, name='status_donasi'),
-    path('formdonasi/',views.form_donasi, name='form_donasi'),
-    path('riwayatdonasi/', views.riwayat_donasi, name='riwayat_donasi')
+    path('admin/', admin.site.urls),
+    path('', include('donation.urls')),  # ini penting!
 ]
