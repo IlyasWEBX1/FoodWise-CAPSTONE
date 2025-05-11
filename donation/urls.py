fdbca6776df3f45b131fac8b2f3e69518ce14f5a
IlyasWEBX1 <l200220131@student.ums.ac.id>
2025-05-11 19:16:03 +0700

    Riwayar donasi donatur fungsional

--- donation/urls.py
+++ donation/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('jadirelawan/', views.jadi_relawan, name='jadi_relawan'),
     path('jadimitra/', views.jadi_mitra, name='jadi_mitra'),
     path('logout/', views.logout_view, name='logout'),
+    path('confirm/<int:transaction_id>/', views.confirm_completion, name='confirm_completion'),
 ]
