824a73e09c6347c3dfa32f960f4ba91c8cb39a17
DavaIlham261 <davamuhammad261@gmail.com>
2025-05-14 12:07:27 +0700

    fungsional dashboard relawan
    1. halaman cari donasi menggunakan desain card untuk tiap item donasi
    2. halaman donasi diambil  menggunakan desain card untuk mengubah status transaksi dan disediakan tabel untuk transaksi selesai

--- relawan/urls.py
+++ relawan/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
   path('caridonasi/', views.caridonasi, name='caridonasi'),
   path('donasidiambil/', views.donasidiambil, name='donasidiambil'),
   path('laporanpembagian/', views.laporanpembagian, name='laporanpembagian'),
+  path('aksi/<int:transaction_id>/', views.aksi_donasi, name='aksi_donasi'),
+
 ]
--- relawan/views.py
+++ relawan/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from donation.models import DonationTransaction, FoodDonor, Volunteer
 
 def caridonasi(request):
-    return render(request, 'relawan/caridonasi.html')
+    username = request.session.get('username')
+    user_role = request.session.get('user_role')
+
+    if user_role == 'Relawan' and username:
+        transactions = DonationTransaction.objects.filter(status = 'Menunggu Diambil')
+        return render(request, 'relawan/caridonasi.html', {'transactions':transactions})
+    else:
+        return redirect('login')
 
 def donasidiambil(request):
-    return render(request, 'relawan/donasidiambil.html')
+    username = request.session.get('username')
+    user_role = request.session.get('user_role')
+    user_id = request.session.get('user_id')
+
+    if user_role == 'Relawan' and username:
+        transactions = DonationTransaction.objects.filter(volunteer_id = user_id)
+        inprogress_transactions = transactions.exclude(status = 'Selesai')
+        completed_transaction = transactions.filter(status = "Selesai")
+        return render(request, 'relawan/donasidiambil.html', {'inprogress_transactions':inprogress_transactions,
+                                                              'completed_transactions':completed_transaction})    
+    else:
+        return redirect('login')
+
 
 def laporanpembagian(request):
     return render(request, 'relawan/laporanpembagian.html')
+
+def aksi_donasi(request, transaction_id):
+    if request.method == 'POST':
+        transaction = get_object_or_404(DonationTransaction, id=transaction_id)
+        if transaction.status == 'Menunggu Diambil': 
+            transaction.volunteer_id = request.session.get('user_id')
+            transaction.status = 'Menunggu Disalurkan' 
+            transaction.save()
+        elif transaction.status == 'Menunggu Disalurkan':
+            transaction.status = 'Telah Disalurkan' 
+            transaction.save()
+        elif transaction.status == 'Telah Disalurkan':
+            transaction.status = 'Selesai' 
+            transaction.save()
+
+    return redirect('donasidiambil')  
